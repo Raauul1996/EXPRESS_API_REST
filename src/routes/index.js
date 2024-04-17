6d47e91c8d36a0e4e0a436b0168724fb6a5bedfc
Raul Gimenez Murga <raulgimenez96@gmail.com>
2024-04-17 05:10:05 +0000

    Delete route made & add gitignore

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,96 +1,88 @@
-const {Router} = require('express');
-const { db } = require ('../firebase.js');
-const crypto = require('node:crypto');
+const { Router } = require('express');
+const { db } = require('../firebase.js');
 const { validateBook, validatePartialBook } = require('../bookSchema.js');
 
 const router = Router();
+const library = db.collection('books');
 
 router.get('/', (req, res) => {
-    res.json('Welcome to my Library API REST')
-})
+    res.json('Welcome to my Library API REST');
+});
 
 router.get('/books', async (req, res) => {
-    const querySnapshot = await db.collection('books').get();
-
+    const querySnapshot = await library.get();
     const allBooks = querySnapshot.docs.map((doc) => ({
         id: doc.id,
         ...doc.data()
-    }))
-    console.log(allBooks);
-    res.status(201).json(allBooks)
+    }));
+    res.status(200).json(allBooks);
 });
 
-/*
-router.get('/api/books', (req, res) => {
-    const { author } = req.query;
-
-    if (author) {
-        console.log(author);
-        const filteredBooks = allBooks.filter(
-            book => book.author.toLowerCase() === author.toLowerCase()
-        );
-        return res.json(filteredBooks);
-    } else {
-        console.log("todos los libros");
-        res.json(allBooks);
-    }
-});
-*/
-
-router.get('/books/:id', async (req, res)=>{
-    const {id} = req.params;
-    const book = await db.collection('books').doc(id).get()
-    if (book) {
+router.get('/books/:id', async (req, res) => {
+    const { id } = req.params;
+    const book = await library.doc(id).get();
+    
+    if (book.data()) {
         const bookData = {
             id: book.id,
             ...book.data()
-        }
-        console.log(bookData)
-        return res.status(200).json(bookData)
+        };
+        res.status(200).json(bookData);
     } else {
-        res.status(404).json({message: 'Book not found'})
+        res.status(404).json({ message: 'Book not found' });
     }
-})
+});
+
+router.delete('/books/:id', async (req, res) => {
+    const { id } = req.params;
+    const book = await library.doc(id).get();
+
+    if (book.data()) {
+        library.doc(id).delete();
+        res.status(200).json({ id, message: 'Book deleted' });
+    } else {
+        res.status(404).json({ message: 'Book not found' });
+    }
+});
 
 router.post('/books', async (req, res) => {
-    const result = validateBook(req.body)
+    const result = validateBook(req.body);
 
-    if(!result.success) {
-        res.status(400).json({error: JSON.parse(result.error.message)})
-
+    if (!result.success) {
+        res.status(400).json({ error: JSON.parse(result.error.message) });
+        return;
     }
-   
+
     const newBook = {
-        //id: crypto.randomUUID(),
+        id: library.doc().id,
         ...result.data
-    }
+    };
 
-    await db.collection('books').add(newBook)
+    await library.doc(newBook.id).set(newBook);
+    res.status(201).json(newBook);
+});
 
-    res.status(201).json(newBook)
-})
+router.put('/books/:id', async (req, res) => {
+    const { id } = req.params;
+    const book = await library.doc(id).get();
 
-router.patch('/books/:id', (req, res)=>{
-    const result = validatePartialBook(req.body)
-    if(!result.success) {
-        res.status(400).json({error: JSON.parse(result.error.message)})
-
-    }
-    const {id} = req.params;
-    const bookIndex = allBooks.findIndex(book => book.id == id)
-    if(bookIndex < 0){
-        res.status(404).json({message: 'Book not found'})
-    } else {
-        const updateBook = {
-            ...allBooks[bookIndex],
-            ...result.data
-        }
+    if (book.data()) {
+        const validateUpdate = validatePartialBook(req.body);
         
-        allBooks[bookIndex] = updateBook //Actualizar DB
+        if (!validateUpdate.success) {
+            res.status(400).json({ error: JSON.parse(validateUpdate.error.message) });
+            return;
+        }
 
-        return res.json(updateBook)
+        const updatedBook = {
+            ...validateUpdate.data
+        };
+
+        await library.doc(id).set(updatedBook, { merge: true });
+        res.status(201).json({message: "Updated book successfully"});
+    } else {
+        res.status(404).json({ message: 'Book not found' });
     }
-})
+});
 
-module.exports = router
-
+module.exports = router;
